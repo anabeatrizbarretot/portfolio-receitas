732c48522c67f2102e32087c27577776116706e0
joaodiasrosa <joaodiasrosa08@gmail.com>
2026-05-13 19:02:45 -0300

    .

--- config/mongo.js
+++ config/mongo.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
-console.log("--- TENTANDO CONEXÃO REFORÇADA AO ATLAS ---");
+// Link Direto (Sem SRV) para pular o erro de DNS do VS Code
+const uri = "mongodb://administrador:administrador@cluster0-shard-00-00.tx6vn46.mongodb.net:27017,cluster0-shard-00-01.tx6vn46.mongodb.net:27017,cluster0-shard-00-02.tx6vn46.mongodb.net:27017/portfolio_receitas?ssl=true&replicaSet=atlas-tx6vn4-shard-0&authSource=admin&retryWrites=true&w=majority&family=4";
 
-// Link padrão (Standard) que funciona melhor em redes com bloqueio
-const uri = "mongodb://administrador:administrador@cluster0-shard-00-00.tx6vn46.mongodb.net:27017,cluster0-shard-00-01.tx6vn46.mongodb.net:27017,cluster0-shard-00-02.tx6vn46.mongodb.net:27017/portfolio_receitas?ssl=true&replicaSet=atlas-tx6vn4-shard-0&authSource=admin&retryWrites=true&w=majority";
+console.log("--- TENTANDO CONEXÃO DIRETA (SEM SRV) ---");
 
 mongoose.connect(uri)
     .then(() => {
-        console.log('✅ SUCESSO ABSOLUTO: MongoDB Atlas Conectado!');
+        console.log('✅ FINALMENTE! Conectado ao MongoDB Atlas via Link Direto!');
     })
     .catch((err) => {
-        console.log('❌ ERRO DE REDE: O MongoDB ainda não consegue te ouvir.');
-        console.error(err.message); // Mostra apenas a mensagem curta do erro
+        console.log('❌ ERRO CRÍTICO:');
+        console.error(err.message);
     });
 
 module.exports = mongoose;
