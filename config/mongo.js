c6a89c4c7c84a4cb5a27466f8a321b1ccbe35427
joaodiasrosa <joaodiasrosa08@gmail.com>
2026-05-13 19:43:59 -0300

    Update mongo.js

--- config/mongo.js
+++ config/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb+srv://administrador:administrador@cluster0.tx6yn46.mongodb.net/test"
+  "mongodb://administrador:administrador@ac-5czhyxl-shard-00-00.tx6vn46.mongodb.net:27017,ac-5czhyxl-shard-00-01.tx6vn46.mongodb.net:27017,ac-5czhyxl-shard-00-02.tx6vn46.mongodb.net:27017/?ssl=true&replicaSet=atlas-bq2xe0-shard-0&authSource=admin&appName=Cluster0"
 )
 .then(() => {
   console.log("✅ Conectado ao MongoDB");
