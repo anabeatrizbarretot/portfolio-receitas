6376c99660931def3942a4d26ad2f4feaf8febec
joaodiasrosa <joaodiasrosa08@gmail.com>
2026-05-13 19:36:57 -0300

    .

--- config/mongo.js
+++ config/mongo.js
@@ -1,19 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-// ESTE LINK NÃO USA SRV, PORTANTO O ERRO "querySrv" NÃO PODE EXISTIR AQUI
-const linkDireto = "mongodb://administrador:administrador@cluster0-shard-00-00.tx6vn46.mongodb.net:27017,cluster0-shard-00-01.tx6vn46.mongodb.net:27017,cluster0-shard-00-02.tx6vn46.mongodb.net:27017/portfolio_receitas?ssl=true&replicaSet=atlas-tx6vn4-shard-0&authSource=admin&retryWrites=true&w=majority";
-
-console.log("--- INICIANDO CONEXÃO MANUAL (LINK LONGO) ---");
-
-mongoose.connect(linkDireto, {
-    family: 4 // Força o uso de IPv4 para evitar erros de IP falso
-})
+mongoose.connect(
+  "mongodb+srv://administrador:administrador@cluster0.tx6yn46.mongodb.net/test"
+)
 .then(() => {
-    console.log('✅ CONECTADO AO ATLAS: O VS Code finalmente venceu a rede!');
+  console.log("✅ Conectado ao MongoDB");
 })
 .catch((err) => {
-    console.log('❌ ERRO REAL DE CONEXÃO:');
-    console.error(err.message);
+  console.log("❌ Erro:");
+  console.log(err);
 });
-
-module.exports = mongoose;
